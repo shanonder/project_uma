224adc8f5620ead49e6bc1b912cc8a9eb7c4c3fb
shanonder <shanonder@DESKTOP-ESF8U4T>
2017-04-30 02:42:49 +0800

    Signed-off-by: shanonder <shanonder@DESKTOP-ESF8U4T>

--- app-net/src/main/java/app/uma/net/config/WebSecurityConfig.java
+++ app-net/src/main/java/app/uma/net/config/WebSecurityConfig.java
@@ -3,12 +3,10 @@ package app.uma.net.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.UserDetailsAwareConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -42,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		
 		auth
-		.userDetailsService(UserDetailsService.class)
+//		.userDetailsService(UserDetailsService.class)
 		.inMemoryAuthentication()
 		.withUser("user").password("password").roles("USER");
 	}
