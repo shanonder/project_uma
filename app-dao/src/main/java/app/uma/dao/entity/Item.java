939d77cf60946254232a05cbc06e10473dd1d688
shanonder <shanonder@163.com>
2017-04-29 14:02:32 +0800

    add:entity.item

--- app-dao/src/main/java/app/uma/dao/entity/Item.java
+++ app-dao/src/main/java/app/uma/dao/entity/Item.java
@@ -1,9 +1,14 @@
 package app.uma.dao.entity;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Item {
@@ -13,14 +18,12 @@ public class Item {
 	private Integer id;
 	
 	private Integer sourceId;
-
-	public Integer getSourceId() {
-		return sourceId;
-	}
-
-	public void setSourceId(Integer sourceId) {
-		this.sourceId = sourceId;
-	}
+	
+	@Lob
+	private String detail;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createTime;
 
 	public Integer getId() {
 		return id;
@@ -30,6 +33,20 @@ public class Item {
 		this.id = id;
 	}
 	
-	
+	public Integer getSourceId() {
+		return sourceId;
+	}
+
+	public void setSourceId(Integer sourceId) {
+		this.sourceId = sourceId;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
 	
 }
