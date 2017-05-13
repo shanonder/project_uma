0e45a40f75fdb05c2ba5e7a4a62ddf7d19f175b5
shanonder <shanonder@DESKTOP-ESF8U4T>
2017-05-13 21:42:45 +0800

    Signed-off-by: shanonder <shanonder@DESKTOP-ESF8U4T>

---
+++ web_uma/src/main/java/app/uma/web/consts/PermisionConst.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * author: shanonder
+ * date: 2017年5月13日 下午5:58:34
+ *
+ */
+package app.uma.web.consts;
+
+public class PermisionConst {
+	public static final String ADMIN = "ADMIN";
+	public static final String MANAGER = "MANAGER";
+	public static final String PROGRAMMER = "PROGRAMMER";
+	public static final String ARTIST = "ARTIST";
+	public static final String DESIGNER = "DESIGNER";
+	public static final String GUEST = "GUEST";
+	
+}
