9099535dc8411056f2ff52620769e9415fba7696
shanonder <shanonder@DESKTOP-ESF8U4T>
2017-05-13 16:44:04 +0800

    Signed-off-by: shanonder <shanonder@DESKTOP-ESF8U4T>

--- web_uma/src/main/java/app/uma/web/config/WebSecurityConfig.java
+++ web_uma/src/main/java/app/uma/web/config/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package app.uma.web.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,64 +12,51 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 import com.qq.open.OpenApiV3;
 
 import app.third.Const;
-import app.uma.util.MD5Util;
-import app.uma.web.AppFilterSecurityInterceptor;
-import app.uma.web.CustomUserService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
+	
+	@Autowired
+	@Qualifier("customUserDetailsService")
+	UserDetailsService userDetailsService;
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Bean
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepositoryImpl = new JdbcTokenRepositoryImpl();
+		tokenRepositoryImpl.setDataSource(dataSource);
+		return tokenRepositoryImpl;
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-			.antMatchers("/guest/**","/auth/**").permitAll()
-			.anyRequest().authenticated()
-//		.and().formLogin()
-//			.loginPage("/guest/login").permitAll()
-//		.and().logout().logoutUrl("/guest/login").permitAll()
-//		.and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/guest/login"))
-//		.and().logout().logoutSuccessUrl("/guest/login").permitAll()
-		.and().csrf()
-			.disable();
-//		http.antMatcher("login").csrf().disable();
-		http.addFilterBefore(appFilterSecurityInterceptor, FilterSecurityInterceptor.class);
+		.antMatchers("/","/auth/**").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password")
+		.and().logout().logoutUrl("/login").permitAll()
+		.and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+		.and().logout().logoutSuccessUrl("/login").permitAll()
+		.and().rememberMe().rememberMeParameter("remember-me").tokenRepository(persistentTokenRepository()).tokenValiditySeconds(86400)
+		.and().csrf().disable();
+//		http.antMatcher("login.html").csrf().disable();
+//		  CsrfTokenResponseHeaderBindingFilter csrfTokenFilter = new CsrfTokenResponseHeaderBindingFilter();
+//		    CustomAccessDeniedHandler accessDeniedHandler=new CustomAccessDeniedHandler();
+//		    http.addFilterAfter(csrfTokenFilter,CsrfFilter.class);
 	}
-
-	//	private Filter ssoFilter() {
-	//		CompositeFilter filter = new CompositeFilter();
-	//		List<Filter> filters = new ArrayList<>();
-	////		filters.add(ssoFilter(facebook(), "/login/facebook"));
-	////		filters.add(ssoFilter(github(), "/login/github"));
-	//		filters.add(tencentFilter("/login/tencent"));
-	//		filter.setFilters(filters);
-	//		return filter;
-	//	}
-
-	@Bean
-	public OpenApiV3 openApi(){
-		return new OpenApiV3(Const.appId, Const.appkey);
-	}
-
-	//
-	//	private Filter tencentFilter(String path) {
-	//		
-	//		return null;
-	//	}
-
-	@Autowired
-	private AppFilterSecurityInterceptor appFilterSecurityInterceptor;
-
+	
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web
@@ -74,30 +64,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers( "/assets/**","/webjars/**"); 
 	}
 
-	@Bean
-	UserDetailsService customUserService(){ //注册UserDetailsService 的bean
-		return new CustomUserService();
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(customUserService()).passwordEncoder(new PasswordEncoder(){
-
-			@Override
-			public String encode(CharSequence rawPassword) {
-				return MD5Util.encode((String)rawPassword);
-			}
-
-			@Override
-			public boolean matches(CharSequence rawPassword, String encodedPassword) {
-				return encodedPassword.equals(MD5Util.encode((String)rawPassword));
-			}}); //user Details Service验证
-
-	}
-
-	private CsrfTokenRepository csrfTokenRepository() {
-		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-		repository.setHeaderName("X-XSRF-TOKEN");
-		return repository;
+	
+	public UserDetailsService detailService;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth
+//		.userDetailsService(UserDetailsService.class)
+		.inMemoryAuthentication()
+		.withUser("user").password("password").roles("USER");
 	}
 }
