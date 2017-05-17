0e9d31db7ff4231d5c651d594b47bb7f30812392
shanonder <shanonder@DESKTOP-ESF8U4T>
2017-05-18 00:07:20 +0800

    Signed-off-by: shanonder <shanonder@DESKTOP-ESF8U4T>

--- web_uma/src/main/java/app/uma/web/config/WebSecurityConfig.java
+++ web_uma/src/main/java/app/uma/web/config/WebSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
