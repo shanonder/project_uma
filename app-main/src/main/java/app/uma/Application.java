803f20a33cd505cc952eae9bee24852f9cc636b6
ShaN <ShaN@ShaN-PC>
2017-05-19 22:13:24 +0800

    Signed-off-by: ShaN <ShaN@ShaN-PC>

--- app-main/src/main/java/app/uma/Application.java
+++ app-main/src/main/java/app/uma/Application.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) throws InterruptedException{
 		SpringApplication.run(Application.class, args);
+		
 	}
 }
