27427cc799743caa091c2d20696faeb670053518
shanonder <shanonder@DESKTOP-ESF8U4T>
2017-05-25 00:01:33 +0800

    Signed-off-by: shanonder <shanonder@DESKTOP-ESF8U4T>

---
+++ web_generate/src/main/java/app/uma/web/generate/Application.java
@@ -0,0 +1,12 @@
+package app.uma.web.generate;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
